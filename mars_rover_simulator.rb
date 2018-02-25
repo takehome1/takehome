d38a5a4265e39b1b9b7bc522e40dadbbc150ad25
Samantha Eng <samengtha+fakegithub@gmail.com>
2018-02-24 20:07:41 -0800

    add input parsing and rover movement

--- mars_rover_simulator.rb
+++ mars_rover_simulator.rb
@@ -2,12 +2,20 @@ require_relative "plateau.rb"
 require_relative "rover.rb"
 
 class MarsRoverSimulator
-  attr_reader :plateau, :rovers
+  ROVER_COUNT = 2
 
   def initialize
+    @plateau = Plateau.new(5, 5)
+    @rovers = []
+
     setup_test_rovers
+    place_all_rovers
     move_all_rovers
   end
+
+  private
+
+  attr_reader :plateau, :rovers
 
   def setup_test_rovers
     @plateau = Plateau.new(5, 5)
@@ -27,12 +35,72 @@ class MarsRoverSimulator
     @rovers << rover_2
   end
 
+  def place_all_rovers
+    rovers.each do |rover|
+      plateau.set_occupied(rover.position)
+    end
+  end
+
   def move_all_rovers
-    # call move_rover on each of the rovers
+    rovers.each_with_index do |rover, i|
+      final_position = move_rover(rover.position, rover.direction, rover.instructions)
+      puts "Rover #{i+1}'s ending position: #{final_position}"
+    end
   end
 
   def move_rover(start_position, start_direction, instructions)
-    # follow each instruction
-    # return end position, end direction
+    old_x, old_y = start_position
+    new_x, new_y = start_position
+    current_direction = start_direction
+
+    instructions.each do |instruction|
+      changed_position = false
+
+      if instruction == "M"
+        changed_position = true
+
+        case current_direction
+        when "N"
+          new_y += 1
+        when "S"
+          new_y -= 1
+        when "E"
+          new_x += 1
+        when "W"
+          new_x -= 1
+        end
+      elsif instruction == "L"
+        case current_direction
+        when "N"
+          current_direction = "W"
+        when "S"
+          current_direction = "E"
+        when "E"
+          current_direction = "N"
+        when "W"
+          current_direction = "S"
+        end
+      elsif instruction == "R"
+        case current_direction
+        when "N"
+          current_direction = "E"
+        when "S"
+          current_direction = "W"
+        when "E"
+          current_direction = "S"
+        when "W"
+          current_direction = "N"
+        end
+      end
+
+      if changed_position
+        plateau.unset_occupied([old_x, old_y])
+        raise "invalid position #{[new_x, new_y]}" unless plateau.valid_position?([new_x, new_y])
+        plateau.set_occupied([new_x, new_y])
+        old_x, old_y = new_x, new_y
+      end
+    end
+
+    "#{new_x} #{new_y} #{current_direction}"
   end
 end
--- plateau.rb
+++ plateau.rb
@@ -1,12 +1,36 @@
-class Plateau
-  attr_reader :x_dimension, :y_dimension
+require "Set"
 
+class Plateau
   def initialize(x_dimension, y_dimension)
     @x_dimension = x_dimension
     @y_dimension = y_dimension
+    @occupied = Set.new
   end
 
   def valid_position?(position)
-    # in bounds and not already occupied(?)
+    !out_of_bounds?(position) && !occupied?(position)
+  end
+
+  def set_occupied(position)
+    @occupied.add(position)
+  end
+
+  def unset_occupied(position)
+    @occupied.delete(position)
+  end
+
+  private
+
+  attr_reader :x_dimension,
+              :y_dimension,
+              :occupied
+
+  def occupied?(position)
+    @occupied.include?(position)
+  end
+
+  def out_of_bounds?(position)
+    x, y = position
+    !(x.between?(0, x_dimension) && y.between?(0, y_dimension))
   end
 end
